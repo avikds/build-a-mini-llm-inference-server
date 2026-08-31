bde91bbe50e89712edf989fa28b39fc49bd66080
Avik Das <avikd957@gmail.com>
2026-08-31 21:30:13 +0530

    Step 26: make_request

--- model.py
+++ model.py
@@ -476,8 +476,15 @@ def kv_blocks_in_use(allocator):
         "total": total,
     }
 
-# Step 26 - make_request (not yet solved)
-# TODO: implement
+# Step 26 - make_request
+def make_request(request_id, prompt_token_ids, max_new_tokens, sampling_params):
+    """Package a single inference request into a dictionary."""
+    return {
+        "request_id": request_id,
+        "prompt_token_ids": list(prompt_token_ids),
+        "max_new_tokens": max_new_tokens,
+        "sampling_params": sampling_params,
+    }
 
 # Step 27 - init_sequence_state (not yet solved)
 # TODO: implement
